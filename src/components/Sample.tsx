fab1e8d5a56b0d760c92ea8f40480cc1050ca0eb
helenapark <phg5590@gmail.com>
2024-08-04 16:09:53 -0400

    fix: change to double quote

--- src/components/Sample.tsx
+++ src/components/Sample.tsx
@@ -17,13 +17,13 @@ const Sample: FC<Props> = ({ text }) => {
       <AccordionItem>
         <h2>
           <AccordionButton>
-            <Box flex='1' textAlign='left' color='brand.primary'>
+            <Box flex="1" textAlign="left" color="brand.primary">
               {text}
             </Box>
             <AccordionIcon />
           </AccordionButton>
         </h2>
-        <AccordionPanel pb={4} color='text.primary'>
+        <AccordionPanel pb={4} color="text.primary">
           This is the content of section 1. Lorem ipsum dolor sit amet,
           consectetur adipiscing elit.
         </AccordionPanel>
@@ -32,13 +32,13 @@ const Sample: FC<Props> = ({ text }) => {
       <AccordionItem>
         <h2>
           <AccordionButton>
-            <Box flex='1' textAlign='left' color='brand.secondary'>
+            <Box flex="1" textAlign="left" color="brand.secondary">
               {text}
             </Box>
             <AccordionIcon />
           </AccordionButton>
         </h2>
-        <AccordionPanel pb={4} color='text.secondary'>
+        <AccordionPanel pb={4} color="text.secondary">
           This is the content of section 2. Lorem ipsum dolor sit amet,
           consectetur adipiscing elit.
         </AccordionPanel>
@@ -47,13 +47,13 @@ const Sample: FC<Props> = ({ text }) => {
       <AccordionItem>
         <h2>
           <AccordionButton>
-            <Box flex='1' textAlign='left' color='brand.tertiary'>
+            <Box flex="1" textAlign="left" color="brand.tertiary">
               {text}
             </Box>
             <AccordionIcon />
           </AccordionButton>
         </h2>
-        <AccordionPanel pb={4} color='text.muted'>
+        <AccordionPanel pb={4} color="text.muted">
           This is the content of section 3. Lorem ipsum dolor sit amet,
           consectetur adipiscing elit.
         </AccordionPanel>
