4b0a444e66743f6dc459527fd44adffd6d6791c8
helenapark <phg5590@gmail.com>
2024-08-04 14:49:19 -0400

    feat: add custom theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,31 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+const fonts = {
+  body: "Spoqa Han Sans, system-ui, sans-serif",
+  weight: {
+    regular: 400,
+    medium: 500,
+    bold: 700,
+  },
+};
+
+const colors = {
+  brand: {
+    primary: "#6448D6",
+    secondary: "#8870EA",
+    tertiary: "#A38FF2",
+    quaternary: "#CABCFF",
+  },
+};
+
+const theme = extendTheme({ fonts, colors });
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
