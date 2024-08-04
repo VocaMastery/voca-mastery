454e740d3f6ce97280d827591fe1defa97dd43dd
helenapark <phg5590@gmail.com>
2024-08-04 16:43:49 -0400

    fix: test error

--- src/components/Sample.tsx
+++ src/components/Sample.tsx
@@ -13,7 +13,7 @@ interface Props {
 }
 const Sample: FC<Props> = ({ text }) => {
   return (
-    <Accordion allowMultiple>
+    <Accordion>
       <AccordionItem>
         <h2>
           <AccordionButton>
@@ -25,36 +25,6 @@ const Sample: FC<Props> = ({ text }) => {
         </h2>
         <AccordionPanel pb={4} color="text.primary">
           This is the content of section 1. Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit.
-        </AccordionPanel>
-      </AccordionItem>
-
-      <AccordionItem>
-        <h2>
-          <AccordionButton>
-            <Box flex="1" textAlign="left" color="brand.secondary">
-              {text}
-            </Box>
-            <AccordionIcon />
-          </AccordionButton>
-        </h2>
-        <AccordionPanel pb={4} color="text.secondary">
-          This is the content of section 2. Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit.
-        </AccordionPanel>
-      </AccordionItem>
-
-      <AccordionItem>
-        <h2>
-          <AccordionButton>
-            <Box flex="1" textAlign="left" color="brand.tertiary">
-              {text}
-            </Box>
-            <AccordionIcon />
-          </AccordionButton>
-        </h2>
-        <AccordionPanel pb={4} color="text.muted">
-          This is the content of section 3. Lorem ipsum dolor sit amet,
           consectetur adipiscing elit.
         </AccordionPanel>
       </AccordionItem>
